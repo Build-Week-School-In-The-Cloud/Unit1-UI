49499f9a77fc1bc2003d0c98058f0ab576c7be21
unknown <ngriffith29@student.egcc.edu>
2020-05-26 20:18:36 -0400

    media qs are working

--- js/app.js
+++
@@ -1,8 +0,0 @@
-import TypeIt from "typeit";
-let h1 =  document.getElementById("demo")
-console.log(h1)
-new TypeIt("#demo", {
-    strings: "This is my string!",
-    speed: 75,
-    loop: true
-  }).go();
